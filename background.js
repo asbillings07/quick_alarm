00cb07bda60fb462704d45ef239166464db1ce06
asbillings07 <superswiftdeveloper@gmail.com>
2020-11-21 12:25:30 -0500

    update background.js

--- background.js
+++ background.js
@@ -8,6 +8,7 @@
     }
 
     calculateDistance = function (distance) {
+      var distance = 
       var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
       var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60))
       var seconds = Math.floor((distance % (1000 * 60)) / 1000)
